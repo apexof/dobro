deba3e91699ba76706b00e210edcf233a329f740
apexof <apexof@gmail.com>
2020-03-10 22:49:57 +0300

    add pagi

---
+++ src/components/catalog/catalog.js
@@ -0,0 +1,3 @@
+$(".catalog-filter__btn").click(() => {
+    $(".catalog-filter-modal").toggle();
+});
--- src/components/order/order.js
+++ src/components/order/order.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
-    $(".order-form-select-region").select2(
-        { minimumResultsForSearch: -1 }
+    $(".select2").select2(
+        {
+            minimumResultsForSearch: -1,
+            width: "100%"
+        }
     );
 });
