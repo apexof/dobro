14ce25cfa22995483c62fdb54a29267d4e9d42ae
apexof <apexof@gmail.com>
2020-03-25 17:14:53 +0300

    fix submenu

--- src/components/main-menu/main-menu.js
+++ src/components/main-menu/main-menu.js
@@ -1,3 +1,10 @@
+const btn = $(".main-menu-el__btn");
 $("#main-menu-button").click(() => {
     $("#main-menu-list").slideToggle("fast");
+    $(".main-menu").toggleClass("main-menu_full-screen");
+});
+$(".main-menu-el__btn").click((e) => {
+    e.preventDefault();
+    btn.parent().next(".main-menu-sub-menu").slideToggle("fast");
+    btn.toggleClass("main-menu-el__btn_close");
 });
